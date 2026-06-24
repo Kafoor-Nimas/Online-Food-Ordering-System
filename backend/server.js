d11b936dfd1904e9afd4129c31ed8a0f1df2ad57
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-06-24 07:29:57 +0530

    Set up backend with Express and MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Server is Live!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
