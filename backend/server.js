0ce449e1cb802cb00a76589f86a57f21d8c7821e
MalshiUdesika <malshigamage15@gmail.com>
2026-06-27 17:20:04 +0530

    feat(server): register user and product routes ✏️

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { createUser, loginUser } from "./controllers/userController.js";
+import userRouter from "./router/userRouter.js";
+import productRouter from "./router/productRouter.js";
 
 dotenv.config();
 connectDB();
@@ -10,6 +13,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+//public routes
+app.post("/api/users", createUser)
+app.post("/api/users/login", loginUser)
+
+app.use("/api/users", userRouter)
+app.use("/api/products", productRouter)
 
 const port = process.env.PORT || 5000;
 
